dfa548439ef71425982ce99656d37c586bb840f1
Manuj Naman <manujnaman@anantak.com>
2025-12-24 15:57:08 -0600

    Added upper arms

--- newton/examples/ANA/LONGBOW/run.py
+++ newton/examples/ANA/LONGBOW/run.py
@@ -4,7 +4,9 @@ import mujoco.viewer
 model = mujoco.MjModel.from_xml_path("./scene.xml")
 data = mujoco.MjData(model)
 
-data.ctrl[0] = 0.0900
-data.ctrl[1] = 0.0900
+data.ctrl[0] = 0.090
+data.ctrl[1] = 0.090
+data.ctrl[2] = 0.130
+data.ctrl[3] = 0.130
 
 mujoco.viewer.launch(model, data, show_left_ui=True, show_right_ui=True)

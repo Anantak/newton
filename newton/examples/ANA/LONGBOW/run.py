29fb93c7222315034b0cf53a721056f5771a98a2
Manuj Naman <manujnaman@anantak.com>
2025-12-24 20:13:41 -0600

    Added forearms and wrists

--- newton/examples/ANA/LONGBOW/run.py
+++ newton/examples/ANA/LONGBOW/run.py
@@ -8,5 +8,9 @@ data.ctrl[0] = 0.090
 data.ctrl[1] = 0.090
 data.ctrl[2] = 0.130
 data.ctrl[3] = 0.130
+data.ctrl[4] = 0.080
+data.ctrl[5] = 0.080
+data.ctrl[6] = 0.050
+data.ctrl[7] = 0.050
 
 mujoco.viewer.launch(model, data, show_left_ui=True, show_right_ui=True)
